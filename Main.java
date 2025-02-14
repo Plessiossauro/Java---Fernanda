159b8e12bcd7acb1e3d5392f5673f4f8f83e8c65
Plessiossauro <166558756+Plessiossauro@users.noreply.github.com>
2025-02-13 21:15:45 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,104 @@
+
+public class Main {
+        public static void main(String[] args) {
+
+                //int num1 = 20;
+                //int num2 = 40;
+                //int soma;
+                //int subtracao;
+                //int multiplicacao;
+
+
+
+                //soma = num1 + num2;
+                //subtracao = num1 - num2;
+                //multiplicacao = num1 * num2;
+
+                //System.out.println(soma);
+                //System.out.println(subtracao);
+                //System.out.println(multiplicacao);
+
+
+
+
+                //if (num1 > num2) {
+                        //System.out.println("é maior");
+                //}else if (num1 == num2) {
+                        //System.out.println("é igual");
+                //}else{
+                        //System.out.println("é menor");
+                //}
+
+
+                //Questão 1
+                int valor_hora = 15;
+                int horas_trabalhadas = 40;
+                int salário = horas_trabalhadas * valor_hora;
+
+                System.out.println(salário);
+
+
+                //Questão 2
+                int valor1 = 25;
+                int valor2 = 25;
+
+
+                if (valor1 > valor2) {
+                        System.out.println("é maior");
+                }else if (valor1 == valor2) {
+                        System.out.println("é igual");
+                }else{
+                        System.out.println("é menor");
+                }
+
+
+                //Questão 3
+                int Nota1 = 70;
+                int Nota2 = 40;
+                int Nota3 = 100;
+                float média = (Nota1+Nota2+Nota3)/3;
+
+                if ( média >= 70) {
+                System.out.println("Aprovado");
+                }else if (média < 40) {
+                System.out.println("Reprovado");
+                }else{
+                System.out.println("Final");
+                }
+
+
+                //Questão 4
+                float folha_de_pagamento = 2050;
+
+                if (folha_de_pagamento <= 900) {
+                System.out.println("ISENTO");
+                }else if (folha_de_pagamento >= 1500) {
+                System.out.println("INCLUSIVE - desconto de 5%");
+                }else if (folha_de_pagamento >= 2500) {
+                System.out.println("INCLUSIVE - desconto de 10%");
+                }else{
+                System.out.println("INCLUSIVE - desconto de 20%");
+                }
+
+
+
+        }
+}
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+//public class Main {
+        //public static void main(String[] args) {
+                // Press Alt+Enter with your caret at the highlighted text to see how
+                // IntelliJ IDEA suggests fixing it.
+                //System.out.println("Hello and welcome!");
+
+
+                // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+                //for (int i = 1; i <= 5; i++) {
+
+                        // Press Shift+F9 to start debugging your code. We have set one breakpoint
+                        // for you, but you can always add more by pressing Ctrl+F8.
+                        //System.out.println("i = " + i);
+                //}
+        //}
+//}
